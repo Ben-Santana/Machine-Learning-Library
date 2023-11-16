83797401ad3a7afbb7b52299f28971df05b146e3
Ben-Santana <101908264+Ben-Santana@users.noreply.github.com>
2023-11-16 15:40:19 -0500

    Initial Commit

---
+++ _test/layer_def.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+# Base Layer:
+# Dense Layer --- Where all nodes in each layer are connected to all nodes in the next layer.
+# 
+# 
+# 
+# 
+# 
+# 
+
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward(self, input):
+        #TODO: return input
+        pass
+
+    def backward(self, output_gradient, learning_rate):
+        #TODO: update parameters and return input gradient
+        pass
+
+class Dense(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.bias = np.random.randn(output_size, 1)
+
+    def forward(self, input):
+        self.input = input
+        return np.dot(self.weights, self.input) + self.bias
+
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+        return input_gradient
+    
+
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+    
+
+class Tanh(Activation):
+    def __init__(self):
+        def tanh(x):
+            return np.tanh(x)
+
+        def tanh_prime(x):
+            return 1 - np.tanh(x) ** 2
+
+        super().__init__(tanh, tanh_prime)
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
---
+++ _test/xor.py
@@ -0,0 +1,40 @@
+from layer_def import Dense, Tanh, mse, mse_prime
+import numpy as np
+from network import train, predict
+
+
+
+#create dataset
+#reshape to make the data 2, 4
+X = np.reshape([[0,0], [0,1], [1,0], [1,1]], (4, 2, 1))
+Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
+
+
+#create network
+network = [
+    Dense(2, 5), 
+    Tanh(), 
+    Dense(5, 1), 
+    Tanh()
+]
+
+#train
+train(network, mse, mse_prime, X, Y, epochs=600000, learning_rate=0.1)
+
+#--------------------------------------------------------------------------------
+
+print("To exit trials at any point type 2")
+
+while True:
+    input_one = int(input("First Value: "))
+    if (input_one == 2): break
+    input_two = int(input("Second Value: "))
+    if (input_one == 2): break
+
+    trial = [input_one, input_two]
+    result = 0;
+    for i in predict(network, trial):
+        for b in i:
+            result += b
+    if( result < 0.5 ): print(0)
+    else: print(1)
