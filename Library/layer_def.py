6802f2ec5690c8de6f43ca858b77b4de68b5943e
Ben-Santana <ben.amaral.santana@gmail.com>
2023-12-15 16:30:20 -0500

    Added changes from _test to main Library

--- Library/layer_def.py
+++ Library/layer_def.py
@@ -1,12 +1,5 @@
 import numpy as np
-
-# Dense Layer --- Where all nodes in each layer are connected to all nodes in the next layer.
-# Activation Layer --- Translates nodes into a value between -1 and 1
-# 
-# 
-# 
-# 
-# 
+import json
 
 class Layer:
     def __init__(self):
@@ -51,7 +44,6 @@ class Activation(Layer):
         return np.multiply(output_gradient, self.activation_prime(self.input))
     
 
-# Activation layer utilizing hyberbolic tangent function
 class Tanh(Activation):
     def __init__(self):
         def tanh(x):
@@ -67,3 +59,33 @@ def mse(y_true, y_pred):
 
 def mse_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / np.size(y_true)
+
+
+#must have a 'saves.json' file
+#saves all weights and biases of a layer array(network) to a saves.json file
+def save(layers, filename='saves.json'):
+    network_data = []
+
+    for layer in layers:
+        layer_data = {}
+        if isinstance(layer, Dense):
+            layer_data["weights"] = layer.weights.tolist()
+            layer_data["bias"] = layer.bias.tolist()
+        elif isinstance(layer, Activation):
+            layer_data["activation"] = layer.activation.__name__
+            layer_data["activation_prime"] = layer.activation_prime.__name__
+        
+        network_data.append(layer_data)
+
+    with open(filename, 'w') as file:
+        json.dump({"network": network_data}, file, indent=2)
+        
+#loads weights and biases onto compatible layer array(network) from saves.json file
+def load(network, filename='saves.json'):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+
+    for layer, layer_data in zip(network, data['network']):
+        if isinstance(layer, Dense):
+            layer.weights = np.array(layer_data["weights"])
+            layer.bias = np.array(layer_data["bias"])
